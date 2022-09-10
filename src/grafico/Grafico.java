6c570ede3fbd6aeb79f8e87c0d67001e30ffb918
Hector Ledezma <73298715+HectorLedezma@users.noreply.github.com>
2022-09-10 13:16:50 -0300

    qe

---
+++ src/grafico/Grafico.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafico;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author hectorledez el gamer
+ */
+
+public class Grafico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Interfaz In = new Interfaz();
+        In.setTitle("Calculadora de IVs");
+        In.setVisible(true);
+    }
+    
+}
